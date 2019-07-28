b7185699b14c29d0ce3289a8c6a6b5867b05ee8a
Edgar <ee1206@hotmail.com>
2019-07-27 23:13:54 -0500

    Initial Commit

---
+++ CurriculumVitae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurriculumVitae
+//
+//  Created by Edgar Escorza on 7/27/19.
+//  Copyright © 2019 Edgar Escorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
